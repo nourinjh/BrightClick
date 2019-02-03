4a27c5b1f280f211a80d4dc4c418795646c6cc55
Nourin Abd El Hadi <nourin.abdelhadi@gmail.com>
2019-02-03 00:40:11 -0500

    Added all files

---
+++ GameStuff/game3/game3.js
@@ -0,0 +1,36 @@
+function check(){
+    
+    var q1 = document.quiz.q1.value;
+    var q2 = document.quiz.q2.value;
+    var q3 = document.quiz.q3.value;
+    var correct = 0;
+
+    if(q1 == "ans"){
+        correct++;
+    }
+    if(q2 == "Edgar Allan Poe"){
+        correct++;
+    }
+    if(q3 == ":"){
+        correct++;
+    }
+
+    var messages = ["Great job, You've earned a point!", "Good effort!"];
+
+    var score;
+
+    if(correct==3){
+        score = 0;
+    }else if(correct<3){
+        score = 1;
+    }
+
+    document.getElementById("after_submit").style.visibility = "visible";
+
+    document.getElementById("message").innerHTML = messages[score];
+    document.getElementById("number_correct").innerHTML = "You got "+correct+" correct.";
+}
+
+// function start(){
+//     document.getElementById("after_submit").style.visibility = "visible";
+// }
---
+++ QuestionBoard/board.js
@@ -0,0 +1,5 @@
+function question(){
+    var question = document.quiz.q1.value;
+
+    document.getElementById("questionn").innerHTML = question;
+}
